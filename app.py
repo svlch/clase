bcef666f0ee565f29a46da21c2fed405225f716f
svlch <117685611+svlch@users.noreply.github.com>
2022-11-11 11:42:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ st.subheader('La jarra del buen beber')
 st.text('Es una guía informativa para la población mexicana que muestra las bebidas')
 st.text('saludables y la cantidad de líquidos que se recomienda consumir al día.')
 st.image('https://cdn.foodandwineespanol.com/2020/09/Jarra_del_buen_Beber.jpg')
+st.latex(r'''
+Vasos=\frac{Peso}{7}
+''')
 st.subheader('El plato el buen comer')
 st.text('Es una guía diseñada por mexicanos para mexicanos con la finalidad de orientar ')
 st.text('a la población hacia una alimentación saludable,')
