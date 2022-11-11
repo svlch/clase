78f2aa358c2d5db0512870aa04297042b6baa134
svlch <117685611+svlch@users.noreply.github.com>
2022-11-11 11:33:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-st.spinner('Cargando...'):
-    time.sleep(5)
-st.success('Listo!')
+import time
+my_bar = st.progress(0)
+for percent_complete in range(100):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1)
 st.title('Comenzando con una buena alimentación')
 st.sidebar.header('¿En qué consiste una buena alimentación?')
 st.sidebar.write('Una alimentación saludable es aquella que proporciona los nutrientes que el cuerpo necesita para mantener el buen funcionamiento del organismo, conservar o restablecer la salud y minimizar el riesgo de enfermedades')
