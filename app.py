f31956c96564710c4264be2f01101f3273b27082
svlch <117685611+svlch@users.noreply.github.com>
2022-11-18 12:42:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.write(cals)
 st.text('El resultado es la cantidad de calorías promedio que necesita el cuerpo para')
 st.text('cumplir las funciones básicas.(Estando en reposo)')
 plato=['Pollo, frijoles, arroz, lechuga, tomate y tortillas de maíz', 'Huevo, frijoles, tortillas de maiz','Pescado, arroz, lechuga, tomate']
-opcioness = st.selectbox('Escoge los alimentos que desees',('Pollo, frijoles, arroz, lechuga, tomate y tortillas de maíz', 'Huevo, frijoles, tortillas de maiz','Pescado, arroz, lechuga, tomate' )
+opcioness=st.selectbox('Escoge los alimentos que desees',('Pollo, frijoles, arroz, lechuga, tomate y tortillas de maíz', 'Huevo, frijoles, tortillas de maiz','Pescado, arroz, lechuga, tomate' ))
 if opcioness=='Pollo, frijoles, arroz, lechuga, tomate y tortillas de maíz':
     st.text('Este platillo contiene 383 calorías')
 st.subheader('¿Para qué tener una buena alimentación?')
