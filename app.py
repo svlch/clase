9f818a2cc060f6a75dcd8ee90c50333063def3e2
svlch <117685611+svlch@users.noreply.github.com>
2022-11-18 13:33:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ if opcioness=='Huevo, frijoles, tortillas de maiz':
 if opcioness=='Pescado, arroz, lechuga, tomate':
     st.text('Este platillo contiene 138 calorías')
 st.subheader('¿Para qué tener una buena alimentación?')
-st.text('El tener una buena alimentación nos proporciona los nutrientes y vitaminas necesarios para que')
-st.text('nuestro organismo logre tener un buen funcionamiento, lograr mantenerlo sano y fortalecer el sistema inmune.')
-
+st.text('El tener una buena alimentación nos proporciona los nutrientes y vitaminas necesarios')
+st.text('para que nuestro organismo logre tener un buen funcionamiento, lograr mantenerlo sano')
+st.text('y fortalecer el sistema inmune.')
 
